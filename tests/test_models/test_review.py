6cfd8ceac0676fb119175697862afa7f1ef5babc
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-09-19 15:04:43 +0300

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,74 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+from models.base_model import BaseModel
+import unittest
+import os
+import pep8
 
 
-class test_review(test_basemodel):
-    """ """
 
-    def __init__(self, *args, **kwargs):
+class TestReview(unittest.TestCase):
+""" """
+    @classmethod
+    def setUpClass(cls):
         """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+        cls.rev1 = Review()
+        cls.rev1.place_id = "Raleigh"
+        cls.rev1.user_id = "Greg"
+        cls.rev1.text = "Grade A"
 
-    def test_place_id(self):
+    @classmethod
+    def tearDownClass(cls):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        del cls.rev1
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
 
-    def test_user_id(self):
+    def test_style_check(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/review.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_text(self):
+    def test_is_subclass(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertTrue(issubclass(self.rev1.__class__, BaseModel), True)
+
+    def test_checking_for_functions(self):
+        """ """
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_has_attributes(self):
+        """ """
+        self.assertTrue('id' in self.rev1.__dict__)
+        self.assertTrue('created_at' in self.rev1.__dict__)
+        self.assertTrue('updated_at' in self.rev1.__dict__)
+        self.assertTrue('place_id' in self.rev1.__dict__)
+        self.assertTrue('text' in self.rev1.__dict__)
+        self.assertTrue('user_id' in self.rev1.__dict__)
+
+    def test_attributes_are_strings(self):
+        """ """
+        self.assertEqual(type(self.rev1.text), str)
+        self.assertEqual(type(self.rev1.place_id), str)
+        self.assertEqual(type(self.rev1.user_id), str)
+
+    @unittest.skipIf(
+        os.getenv('HBNB_TYPE_STORAGE') == 'db',
+        "won't work in db")
+
+    def test_save(self):
+        """ """
+        self.rev1.save()
+        self.assertNotEqual(self.rev1.created_at, self.rev1.updated_at)
+
+    def test_to_dict(self):
+        """ """
+        self.assertEqual('to_dict' in dir(self.rev1), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
