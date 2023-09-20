d1f79877154cf9baff447246c3f43212ea199ada
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-09-20 14:52:45 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,7 +15,6 @@ class TestBaseModel(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-    """ """
         del cls.base1
         try:
             os.remove("file.json")
@@ -23,37 +22,31 @@ class TestBaseModel(unittest.TestCase):
             pass
 
     def test_style_check(self):
-     """ """
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/base_model.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_checking_for_functions(self):
-    """ """
         self.assertIsNotNone(BaseModel.__doc__)
         self.assertIsNotNone(BaseModel.save.__doc__)
         self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
     def test_attributes(self):
-    """ """
         self.assertTrue(hasattr(BaseModel, "__init__"))
         self.assertTrue(hasattr(BaseModel, "save"))
         self.assertTrue(hasattr(BaseModel, "to_dict"))
 
     def test_init(self):
-    """ """
         self.assertTrue(isinstance(self.base1, BaseModel))
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
                      "won't work in db")
 
     def test_save(self):
-    """ """
         self.base1.save()
         self.assertNotEqual(self.base1.created_at, self.base1.updated_at)
 
     def test_to_dict(self):
-    """ """
         base1_dict = self.base1.to_dict()
         self.assertEqual(self.base1.__class__.__name__, 'BaseModel')
         self.assertIsInstance(base1_dict['created_at'], str)
